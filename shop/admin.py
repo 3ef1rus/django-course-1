0ce6610ec829ae21c3d68b96467dd41fa76cb3d5
3e1rus <ivanvyal321@gmail.com>
2024-02-01 17:44:21 +0100

    customize admin page

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,33 @@
 from django.contrib import admin
-from . import models
+from .models import Category, Course
 # Register your models here.
+# 3 строчки ниже настраивают текст на админ панели
+admin.site.site_header = "Store admin panel"
+admin.site.site_title = "Shop"
+admin.site.index_title = "Welcome to the admin"
 
-admin.site.register(models.Category)
-admin.site.register(models.Course)
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('titel', 'price', 'category', 'id')
+
+
+class CoursesInline(admin.TabularInline):
+    model = Course
+    exclude = ['created_at']
+    extra = 1
+
+
+class CategoruAdmin(admin.ModelAdmin):
+    list_display = ('titel', 'created_at')
+    fieldsets = [
+        (None, {'fields': ['titel']}),
+        ('Dates', {
+            'fields': ['created_at'],
+            'classes': ['collapse']
+        })
+    ]
+    inlines = [CoursesInline]
+
+
+admin.site.register(Category, CategoruAdmin)
+admin.site.register(Course, CourseAdmin)
