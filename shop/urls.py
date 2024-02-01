64b2aaafe060d7ceefeb5be0b2597aa160c0b4f6
3e1rus <ivanvyal321@gmail.com>
2024-02-01 13:38:58 +0100

    create new page and add it in path

--- shop/urls.py
+++ shop/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+app_name = 'shop'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:course_id>', views.singel_course, name='single_course')
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from .models import Course
+
+
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello from shop app")
+    courses = Course.objects.all()
+    # return HttpResponse(''.join([str(el)+'<br>' for el in courses]))
+    return render(request, 'courses.html', {'courses': courses})
+
+
+def singel_course(request, course_id):
+    # Option 1
+    # try:
+    #     course = Course.objects.get(pk=course_id)
+    #     return render(request, 'single_course.html', {'course': course})
+    # except Course.DoesNotExist:
+    #     raise Http404()
+    # Option 2
+
+    course = get_object_or_404(Course, pk=course_id)
+    return render(request, 'single_course.html', {'course': course})
